9d96e1119b383c02ff0ebfb6dd323c2b8c805e97
tagizadea <tagizadea01@gmail.com>
2025-02-26 13:18:36 +0400

    Update Department.java

--- model/Department.java
+++ model/Department.java
@@ -1,5 +1,31 @@
 package model;
 
-public class Department {
-    
+public abstract class Department {
+
+    private String departmentName;
+    private int departmentId;
+
+    public Department(String departmentName, int departmentId) {
+        this.departmentName = departmentName;
+        this.departmentId = departmentId;
+    }
+
+    public abstract void manageDepartment();
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
 }
+
